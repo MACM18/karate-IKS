d988e7c12b32d8a7b17d5dc181ae6613c2df9111
Chathura <hello@macm.dev>
2026-01-23 18:52:19 +0530

    more fixes

---
+++ eslint.config.cjs
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    ignores: [".next/**", "node_modules/**"],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      react: require("eslint-plugin-react"),
+      "react-hooks": require("eslint-plugin-react-hooks"),
+      "jsx-a11y": require("eslint-plugin-jsx-a11y"),
+    },
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    rules: {
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
